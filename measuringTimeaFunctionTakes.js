b71c9c8625439c64c4f1020f3079300549a2713c
Georges Le Vaillant <44489447+stonefarmer9@users.noreply.github.com>
2019-09-05 09:20:22 +0100

    Update and rename algorithims.js to measuringTimeaFunctionTakes.js

--- measuringTimeaFunctionTakes.js
+++ measuringTimeaFunctionTakes.js
@@ -3,7 +3,7 @@ const performance = require('perf_hooks').performance;
 function sum(total, number) {
   return total + number
 }
-
+//begin timing now
 var t = performance.now()
 
 var b = [1,2,3,4,5]
@@ -11,4 +11,7 @@ var b = [1,2,3,4,5]
 b.reduce(sum)
 
 var t1 = performance.now()
+// end timing of operation
 console.log("Call to doSomething took " + (t1 - t) + " milliseconds.");
+// we find out how long it took by subtracting the end result from the first result. 
+// this takes two timestamps which allows us to measure how long something took.
